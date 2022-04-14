06c89be46bb5e8bb48f5d496b93fb5fedb943550
Troy Weaver <troylw@gmail.com>
2022-04-14 14:45:06 -0400

    Add project files.

---
+++ Models/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Pokedex.BlazorWASM.Models
+{
+    public class Pokemon
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("weight")]
+        public int Weight { get; set; }
+        [JsonProperty("height")]
+        public int Height { get; set; }
+        [JsonProperty("base_experience")]
+        public int Experience { get; set; }
+
+    }
+}
